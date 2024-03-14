34031ee9321c4bb4d5c687604553df91a85c0b8d
singh0903diksha <117086804+singh0903diksha@users.noreply.github.com>
2024-03-14 11:59:46 +0530

    Update script.js
    changed API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "a70f616888884c0398e262149daa22af";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
